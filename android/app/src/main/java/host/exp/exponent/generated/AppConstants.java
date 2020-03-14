c115dc143f4d8f620899cf1bf6b86c00984ce09d
InSooBae <qospwmf@gmail.com>
2020-03-14 16:14:47 +0900

    android

---
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -0,0 +1,49 @@
+package host.exp.exponent.generated;
+
+import com.facebook.common.internal.DoNotStrip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import host.exp.exponent.BuildConfig;
+import host.exp.exponent.Constants;
+
+@DoNotStrip
+public class AppConstants {
+
+  public static final String VERSION_NAME = "1.0.0";
+  public static String INITIAL_URL = "exp://exp.host/@insoo/HURE-Contact";
+  public static final String SHELL_APP_SCHEME = "exp99aa17ba68ab4b9a8b1f5dda8a4cced4";
+  public static final String RELEASE_CHANNEL = "default";
+  public static boolean SHOW_LOADING_VIEW_IN_SHELL_APP = true;
+  public static boolean ARE_REMOTE_UPDATES_ENABLED = true;
+  public static final List<Constants.EmbeddedResponse> EMBEDDED_RESPONSES;
+  public static boolean FCM_ENABLED = false;
+
+  static {
+    List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
+
+    
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@insoo/HURE-Contact", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40insoo%2FHURE-Contact%2F1.0.0%2F7e7fce5f6c52439dedf54f64c651693c-36.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
+    EMBEDDED_RESPONSES = embeddedResponses;
+  }
+
+  // Called from expoview/Constants
+  public static Constants.ExpoViewAppConstants get() {
+    Constants.ExpoViewAppConstants constants = new Constants.ExpoViewAppConstants();
+    constants.VERSION_NAME = VERSION_NAME;
+    constants.INITIAL_URL = INITIAL_URL;
+    constants.SHELL_APP_SCHEME = SHELL_APP_SCHEME;
+    constants.RELEASE_CHANNEL = RELEASE_CHANNEL;
+    constants.SHOW_LOADING_VIEW_IN_SHELL_APP = SHOW_LOADING_VIEW_IN_SHELL_APP;
+    constants.ARE_REMOTE_UPDATES_ENABLED = ARE_REMOTE_UPDATES_ENABLED;
+    constants.EMBEDDED_RESPONSES = EMBEDDED_RESPONSES;
+    constants.ANDROID_VERSION_CODE = BuildConfig.VERSION_CODE;
+    constants.FCM_ENABLED = FCM_ENABLED;
+    return constants;
+  }
+}
---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp99aa17ba68ab4b9a8b1f5dda8a4cced4://192.168.35.48:80";
+
+}
